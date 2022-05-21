833c9433a1020a1e8ad92672f7ef2d0445ed1c5f
daniel <cdtenzi@gmail.com>
2022-05-21 12:50:11 +0200

    checkout page + funcionalidad

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,28 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { CartContext } from "../../contexts/cart.context";
+
+import CartItem from "../cart-item/cart-item.component";
 import Button from "../button/button.component";
+
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate("/checkout");
+  };
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items"></div>
-      <Button>Check out</Button>
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
+      </div>
+      <Button onClick={goToCheckoutHandler}>Check out</Button>
     </div>
   );
 };
---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,19 @@
+import { SnapshotMetadata } from "firebase/firestore";
+import "./cart.item.styles.scss";
+
+const CartItem = ({ cartItem }) => {
+  const { id, name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div key={id} className="cart-item-container">
+      <img src={imageUrl} alt={`${name}`} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
