2ce2b2a4d10d5abb6e0e947064fc750c7da96fb1
daniel <cdtenzi@gmail.com>
2022-07-26 20:25:27 +0200

    sign up sagas

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,12 +1,10 @@
 import { SignUpContainer } from "./sign-up-form.styles";
 import { useState } from "react";
-import {
-  createAuthUserWithEmailAndPassword,
-  createUserDocumentFromAuth,
-} from "../../utils/firebase/firebase.utils";
+import { useDispatch } from "react-redux";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
+import { signUpStart } from "../../store/user/user.action";
 
 const defaultFormFields = {
   displayName: "",
@@ -18,6 +16,7 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
+  const dispatch = useDispatch();
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -32,18 +31,7 @@ const SignUpForm = () => {
 
     if (formFields.password === formFields.confirmPassword) {
       try {
-        // este es el usuario que devuelve la librería de autenticación. Este usuario en los casos que la Auth
-        // viene de google, es devuelto con toda la info que tiene google (incluido el displayname). Para el
-        // caso en que el usuario se registra con mail y pass, firebase lo autentica pero no tiene más info que
-        // el mail y pass que le fue proporcionado para autenticar.
-        const { user } = await createAuthUserWithEmailAndPassword(
-          email,
-          password,
-          displayName
-        );
-        // Por el motivo anterior, usamos el valor displayname que proporcionó el usario para guardarlo en
-        // nuestra base de datos.
-        await createUserDocumentFromAuth(user, { displayName });
+        dispatch(signUpStart(email, password, displayName));
         resetFormFields();
       } catch (err) {
         console.log(err);
